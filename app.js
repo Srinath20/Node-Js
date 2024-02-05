fff58dd61f6ced08bdf244817e9427ff823c5f72
Srinath20 <srinathgollapinni>
2024-02-05 23:13:07 +0530

    Push

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const server = http.createServer((req,res) =>{
+    console.log("Srinath");
+})
+server.listen(4000);
+
+/*
+1)Internet is computers interconnected worldwide. It follows some rules(protocols) in which IP (Internet protocol) being most fundamental protocol to communicate bewtween devices.
+
+
+
+2)HTTP,FS,PATH , EVENTS
+
+3)HTTP:This module allows transfer of data between node.js and computer throught hyper text transfer protocol.
+
+   FS: File system allows node.js to interact with file systems on the computer its running.
+
+	PATH : It allows to manipulate with the files.
+
+	EVENTS : It helps in creating custome events and event listeners in node.js
+
+
+
+
+*/
