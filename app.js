b34457c3ba198d8e482fe8dbf67c9268cf3f6ce0
Srinath20 <srinathgollapinni>
2024-02-14 16:18:13 +0530

    Responses

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const http = require('http');
-const server = http.createServer((req,res) =>{
-    console.log("Srinath");
-})
-server.listen(4000);
+const fs = require('fs');
+fs.readFile('hello.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error("Error reading messages:", err);
+        return;
+    }
+    messages = data.split('\n').filter(Boolean);
+});
+
+http.createServer(function(req, res) {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<h1>Messages:</h1>');
+    res.write('<ul>');
+    messages.forEach(message => {
+        res.write('<li>' + message + '</li>');
+    });
+    res.write('</ul>');
+
+    res.write('<h1>Welcome to my page</h1>');
+    res.write('<form method="POST" action="/message">');
+    res.write('<input type="text" name="message">');
+    res.write('<button type="submit">Send</button>');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+}).listen(9010);
+
 
 /*
 1)Internet is computers interconnected worldwide. It follows some rules(protocols) in which IP (Internet protocol) being most fundamental protocol to communicate bewtween devices.
